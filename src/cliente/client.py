a997b7831506b23817a447eb164608e0f32e536e
Vanakaus <vinicius_vnk@hotmail.com>
2022-11-13 04:12:28 -0300

    Criando algoritmo para o jogo

--- src/cliente/client.py
+++ src/cliente/client.py
@@ -1,8 +1,39 @@
 import tkinter as tk
+from random import shuffle
 
 window = tk.Tk()
-cartas = [[0 for x in range(6)] for x in range(6)]
 bg_quadro = "#fefefe"
+bg_carta = "#e8c999"
+pontos = 0
+lbl_pontuacao2 = tk.Label()
+lbl_listPlayers = tk.Label()
+cartas = [[0 for x in range(6)] for x in range(6)]
+carta1 = [-1, -1]
+carta2 = [-1, -1]
+cores = [""] * 18
+seq_cores = [""] * 36
+
+cores[0] = "#DD0000"
+cores[1] = "#FFA500"
+cores[2] = "#FFC0CB"
+cores[3] = "#A020F0"
+cores[4] = "#3030FF"
+cores[5] = "#00FFFF"
+cores[6] = "#ADD8E6"
+cores[7] = "#FFFF00"
+cores[8] = "#00FF00"
+cores[9] = "#00AA00"
+cores[10] = "#964B00"
+cores[11] = "#FFDB58"
+cores[12] = "#03bb85"
+cores[13] = "#C4A484"
+cores[14] = "#FFFFFF"
+cores[15] = "#D3D3D3"
+cores[16] = "#808080"
+cores[17] = "#000000"
+
+
+
 
 
 def janela():
@@ -21,17 +52,19 @@ def pontuacao():
 
     lbl_pontuacao = tk.Label(text="Sua Pontuação:", bg=bg_quadro, font=("arial", 20))
     lbl_pontuacao.place(x=20, y=100)
-    lbl_pontuacao2 = tk.Label(text="5", bg=bg_quadro, font=("arial", 20))
+    global lbl_pontuacao2
+    lbl_pontuacao2 = tk.Label(text=pontos, bg=bg_quadro, font=("arial", 20))
     lbl_pontuacao2.place(x=25, y=130)
 
 def players():
     mesa2 = tk.Frame(window, width=200, height=280, bg=bg_quadro)
     mesa2.place(x=20, y=200)
 
-    lbl_players = tk.Label(text="Jogadores:", bg=bg_quadro, font=("arial", 22))
+    lbl_players = tk.Label(text="Jogadores", bg=bg_quadro, font=("arial", 22))
     lbl_players.place(x=40, y=200)
 
-    lbl_listPlayers = tk.Label(text="João Vitor - 8 \nVinicius - 5", bg=bg_quadro, font=("arial", 18), justify="left")
+    global lbl_listplayers
+    lbl_listPlayers = tk.Label(text="João Vitor: 8 \nVinicius: 5", bg=bg_quadro, font=("arial", 18), justify="left")
     lbl_listPlayers.place(x=25, y=250)
 
 def mesa():
@@ -41,9 +74,62 @@ def mesa():
     mesa2.place(x=75, y=7)
     for i in range(6):
         for j in range(6):
-            cartas[i][j] = tk.Button(mesa2, width=4, height=2, cursor="hand2", bg="#e8c999", activebackground="#e8c999", command=lambda x1=i, y1=j: retirar(x1,y1))
+            cartas[i][j] = tk.Button(mesa2, width=4, height=2, cursor="hand2", bg=bg_carta, activebackground=bg_carta, command=lambda x1=i, y1=j: Escolher(x1,y1))
             cartas[i][j].grid(column=j, row=i, padx=12, pady=10)
 
+
+def sortear():
+    for i in range(18):
+        seq_cores[i] = cores[i]
+        seq_cores[i+18] = cores[i]
+    shuffle(seq_cores)
+
+
+def atrribuir_cores():
+    x = 0
+    for i in range(6):
+        for j in range(6):
+            cartas[i][j].config(bg=seq_cores[x])
+            x = x + 1
+
+def Escolher(y, x):
+    print("Carta [{}][{}] escolhida".format(x, y))
+    print("x", x)
+    print("y", y)
+    print("Cor: ", y*6 + x)
+
+    cartas[y][x].config(bg=seq_cores[y*6 + x])
+    cartas[y][x].config(state="disabled")
+    if carta1[0] == -1 and carta1[1] == -1:
+        carta1[0] = x
+        carta1[1] = y
+    elif carta2[0] == -1 and carta2[1] == -1:
+        carta2[0] = x
+        carta2[1] = y
+
+        window.update()
+        window.after(1000)
+
+        if seq_cores[carta1[0] + carta1[1]*6] == seq_cores[carta2[0] + carta2[1]*6]:
+            print("Acertou")
+            cartas[carta1[1]][carta1[0]].destroy()
+            cartas[carta2[1]][carta2[0]].destroy()
+            carta1[0] = -1
+            carta1[1] = -1
+            carta2[0] = -1
+            carta2[1] = -1
+
+            global pontos
+            pontos = pontos + 1
+            lbl_pontuacao2.config(text=pontos)
+        else:
+            print("Errou")
+            cartas[carta1[1]][carta1[0]].config(bg=bg_carta, state="normal")
+            cartas[carta2[1]][carta2[0]].config(bg=bg_carta, state="normal")
+            carta1[0] = -1
+            carta1[1] = -1
+            carta2[0] = -1
+            carta2[1] = -1
 
 
 def retirar(x, y):
@@ -58,8 +144,12 @@ def retirar(x, y):
 
 if __name__ == "__main__":
     print("Iniciando cliente")
+
     janela()
     pontuacao()
     players()
     mesa()
+    sortear()
+    atrribuir_cores()
+
     window.mainloop()
